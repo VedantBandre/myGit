66200350dd2f0fbd4fdf7702838f2a44cd8ca4ba
VedantBandre <vedantbandre33@gmail.com>
2025-10-26 02:42:55 +0100

    tree write

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(name = 'mygit',
+       version = '1.0',
+       packages = ['mygit'],
+       entry_points = {
+           'console_scripts' : [
+               'mygit = mygit.cli:main'
+        ]
+    }
+)
